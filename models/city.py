91629430f8cac1d1a310ef09ae2a32a70910caff
steve9948 <mbuguagichimu@gmail.com>
2023-12-12 17:56:20 +0000

    Added city

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    module: City
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ Defines City class """
+    state_id = ""
+    name = ""
